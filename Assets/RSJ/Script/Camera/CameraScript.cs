5ae7966dbd3ca514783d5e137acf64d6e20cc275
moon080411 <157565761+moon080411@users.noreply.github.com>
2026-04-23 17:39:46 +0900

    feat : CamClipObject

--- Assets/RSJ/Script/Camera/CameraScript.cs
+++ Assets/RSJ/Script/Camera/CameraScript.cs
@@ -18,7 +18,7 @@ public struct CopyedObj
 
     public void ChangeTransform(Vector2 camPos)
     {
-        if (_copyObj)
+        if (_copyObj)   
         {
             _copyObj.transform.position = camPos + _camToPos;
         }
@@ -34,11 +34,11 @@ public class CameraScript : MonoBehaviour
     private RectTransform myPosition;
     private Vector3 _position;
     private MyColor _nowColor;
-    
+
     private bool _copying = false;
 
     private Dictionary<GameObject, CopyedObj> _copyObjs = new();
-    
+
     private void Awake()
     {
         myPosition = GetComponent<RectTransform>();
@@ -47,11 +47,12 @@ public class CameraScript : MonoBehaviour
 
     private void Update()
     {
-        _position = new Vector3(Input.mousePosition.x - (myPosition.sizeDelta.x / 2), Input.mousePosition.y - (myPosition.sizeDelta.y / 2));
+        _position = new Vector3(Input.mousePosition.x - (myPosition.sizeDelta.x / 2),
+            Input.mousePosition.y - (myPosition.sizeDelta.y / 2));
         myPosition.anchoredPosition = _position;
 
         Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        
+
         if (_copying)
         {
             foreach (var camObj in _copyObjs)
@@ -59,7 +60,7 @@ public class CameraScript : MonoBehaviour
                 camObj.Value.ChangeTransform(pos);
             }
         }
-        
+
         if (Keyboard.current.gKey.wasPressedThisFrame)
         {
             if (_copying)
@@ -86,6 +87,11 @@ public class CameraScript : MonoBehaviour
             Debug.Log("찰칵");
         }
     }
+    
+    private void PressObj()
+    {
+        StopCopy(false);
+    }
 
     private void StopCopy(bool b = true)
     {
@@ -94,19 +100,27 @@ public class CameraScript : MonoBehaviour
         {
             foreach (var camObj in _copyObjs)
             {
-                if(camObj.Value._copyObj)
+                if (camObj.Value._copyObj)
                 {
                     Destroy(camObj.Value._copyObj);
                 }
             }
         }
+        else
+        {
+            foreach (var camObj in _copyObjs)
+            {
+                camObj.Value._copyObj.GetComponent<Collider2D>().enabled = true;
+            }
+        }
+
         _copyObjs.Clear();
     }
 
     private void CopyObj(Vector2 pos)
     {
         Collider2D[] items = Physics2D.OverlapBoxAll(checkPos.position, (myPosition.sizeDelta / 110), 0, coloredObject);
-    
+
         if (items == null) return;
 
         Vector2 checkMin = (Vector2)checkPos.position - myPosition.sizeDelta / 110 / 2;
@@ -117,13 +131,53 @@ public class CameraScript : MonoBehaviour
             Vector2 itemMin = item.bounds.min;
             Vector2 itemMax = item.bounds.max;
 
-            if (itemMin.x < checkMin.x || itemMin.y < checkMin.y ||
-                itemMax.x > checkMax.x || itemMax.y > checkMax.y)
+            if (itemMax.x < checkMin.x || itemMin.x > checkMax.x ||
+                itemMax.y < checkMin.y || itemMin.y > checkMax.y)
                 continue;
 
-            GameObject obj = Instantiate(item.gameObject);
-            obj.transform.position = item.transform.position;
-            _copyObjs.Add(item.gameObject, new CopyedObj((Vector2)item.transform.position - pos, obj));
+            Vector2 clippedMin = new Vector2(
+                Mathf.Max(itemMin.x, checkMin.x),
+                Mathf.Max(itemMin.y, checkMin.y)
+            );
+            Vector2 clippedMax = new Vector2(
+                Mathf.Min(itemMax.x, checkMax.x),
+                Mathf.Min(itemMax.y, checkMax.y)
+            );
+
+            SpriteRenderer sr = item.GetComponent<SpriteRenderer>();
+            if (sr == null || sr.sprite == null) continue;
+            
+            bool inBox = (itemMin.x >= checkMin.x && itemMax.x <= checkMax.x && itemMin.y >= checkMin.y && itemMax.y <= checkMax.y);
+            
+            GameObject obj;
+            Vector2 realCenter;
+            
+            if (inBox)
+            {
+                obj = Instantiate(item.gameObject);
+                SpriteRenderer newSr = obj.GetComponent<SpriteRenderer>();
+                newSr.sortingOrder = sr.sortingOrder + 1;
+            
+                realCenter = obj.transform.position; 
+            }
+            else
+            {
+                Sprite clippedSprite = ClipSprite(sr, clippedMin, clippedMax);
+                if (clippedSprite == null) continue;
+
+                obj = Instantiate(item.gameObject);
+                SpriteRenderer newSr = obj.GetComponent<SpriteRenderer>();
+                newSr.sprite = clippedSprite;
+                newSr.sortingOrder = sr.sortingOrder + 1;
+
+                realCenter = (clippedMin + clippedMax) / 2f;
+                obj.transform.position = realCenter;
+
+                ReplaceClippedCollider(obj, clippedMin, clippedMax);
+            }
+
+            obj.GetComponent<Collider2D>().enabled = false;
+            _copyObjs.Add(item.gameObject, new CopyedObj(realCenter - pos, obj));
         }
 
         if (_copyObjs.Count == 0)
@@ -132,15 +186,84 @@ public class CameraScript : MonoBehaviour
         _copying = true;
     }
 
-    private void PressObj()
+    private void ReplaceClippedCollider(GameObject obj, Vector2 worldMin, Vector2 worldMax)
     {
-        StopCopy(false);
+        foreach (var col in obj.GetComponents<Collider2D>())
+            DestroyImmediate(col);
+
+        BoxCollider2D newCol = obj.AddComponent<BoxCollider2D>();
+        newCol.size = obj.transform.InverseTransformVector(worldMax - worldMin);
+        newCol.offset = Vector2.zero;
+    }
+
+    private Sprite ClipSprite(SpriteRenderer sr, Vector2 worldMin, Vector2 worldMax)
+    {
+        Sprite original = sr.sprite;
+        Texture2D originalTex = original.texture;
+
+        Texture2D readableTex = GetReadableTexture(originalTex);
+
+        float ppu = original.pixelsPerUnit;
+
+        Vector2 objWorldMin = sr.bounds.min;
+        Vector2 objWorldSize = sr.bounds.size;
+
+        Rect spriteRect = original.textureRect;
+
+        float u0 = (worldMin.x - objWorldMin.x) / objWorldSize.x;
+        float v0 = (worldMin.y - objWorldMin.y) / objWorldSize.y;
+        float u1 = (worldMax.x - objWorldMin.x) / objWorldSize.x;
+        float v1 = (worldMax.y - objWorldMin.y) / objWorldSize.y;
+
+        int px = Mathf.RoundToInt(spriteRect.x + u0 * spriteRect.width);
+        int py = Mathf.RoundToInt(spriteRect.y + v0 * spriteRect.height);
+        int pw = Mathf.RoundToInt((u1 - u0) * spriteRect.width);
+        int ph = Mathf.RoundToInt((v1 - v0) * spriteRect.height);
+
+        if (pw <= 0 || ph <= 0) return null;
+
+        px = Mathf.Clamp(px, 0, readableTex.width - 1);
+        py = Mathf.Clamp(py, 0, readableTex.height - 1);
+        pw = Mathf.Clamp(pw, 1, readableTex.width - px);
+        ph = Mathf.Clamp(ph, 1, readableTex.height - py);
+
+        Color[] pixels = readableTex.GetPixels(px, py, pw, ph);
+        Texture2D newTex = new Texture2D(pw, ph, TextureFormat.RGBA32, false);
+        newTex.filterMode = originalTex.filterMode;
+        newTex.SetPixels(pixels);
+        newTex.Apply();
+
+        Sprite newSprite = Sprite.Create(
+            newTex,
+            new Rect(0, 0, pw, ph),
+            new Vector2(0.5f, 0.5f),
+            ppu
+        );
+
+        return newSprite;
+    }
+
+    private Texture2D GetReadableTexture(Texture2D source)
+    {
+        RenderTexture rt = RenderTexture.GetTemporary(source.width, source.height, 0, RenderTextureFormat.Default,
+            RenderTextureReadWrite.Linear);
+        Graphics.Blit(source, rt);
+        RenderTexture prev = RenderTexture.active;
+        RenderTexture.active = rt;
+
+        Texture2D readable = new Texture2D(source.width, source.height);
+        readable.ReadPixels(new Rect(0, 0, source.width, source.height), 0, 0);
+        readable.Apply();
+
+        RenderTexture.active = prev;
+        RenderTexture.ReleaseTemporary(rt);
+        return readable;
     }
 
     private void CheckObj()
-    {    
+    {
         Collider2D item = Physics2D.OverlapBox(checkPos.position, (myPosition.sizeDelta / 110), 0, coloredObject);
-        
+
         if (item != null && item.gameObject.TryGetComponent(out ColoredObject colorObj))
         {
             _nowColor = colorObj.color;
