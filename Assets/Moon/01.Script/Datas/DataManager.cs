8f5b8eb55474ee46f72e4e50a54c43e7616a2aa8
moon080411 <157565761+moon080411@users.noreply.github.com>
2026-04-27 20:12:08 +0900

    feat : DataManager

---
+++ Assets/Moon/01.Script/Datas/DataManager.cs
@@ -0,0 +1,242 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using MoonLib.ScriptFinder_Pro.RunTime.DevLogs;
+using UnityEngine;
+
+namespace Moon._01.Script.Datas
+{
+    [DefaultExecutionOrder(-75)]
+    public class DataManager : MonoBehaviour
+    {
+        public static DataManager Instance { get; private set; }
+
+        [field: SerializeField] public int MaxSaveSlot { get; private set; } = 3;
+        
+        private Dictionary<string, int> _currentData = new Dictionary<string, int>();
+        
+        private Dictionary<string, int> _autoSaved = new Dictionary<string, int>();
+
+        private List<Dictionary<string, int>> allData = new List<Dictionary<string, int>>();
+        
+        private List<bool> _slotDataExist = new List<bool>();
+
+        private bool autoDataExist;
+        
+        private string _path;
+        
+#region Init
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+                Init();
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        private void Init()
+        {
+            _path = Application.persistentDataPath + "/Data";
+            if (!Directory.Exists(_path))
+            {
+                Directory.CreateDirectory(_path);
+            }
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            try
+            {
+                for (int i = 0; i < MaxSaveSlot; i++)
+                {
+                    string filePath = $"{_path}/save_{i}.json";
+                    if (File.Exists(filePath))
+                    {
+                        string jsonData = File.ReadAllText(filePath);
+                        Dictionary<string, int> data = DictionaryJsonConvert.FromJson<string, int>(jsonData);
+                        allData.Add(data);
+                        _slotDataExist.Add(true);
+                    }
+                    else
+                    {
+                        _slotDataExist.Add(false);
+                        allData.Add(new Dictionary<string, int>());
+                    }
+                }
+
+                string autoFilePath = $"{_path}/save_auto.json";
+                if (File.Exists(autoFilePath))
+                {
+                    string jsonData = File.ReadAllText(autoFilePath);
+                    _autoSaved = DictionaryJsonConvert.FromJson<string, int>(jsonData);
+                    autoDataExist = true;
+                }
+                else
+                {
+                    autoDataExist = false;
+                }
+            }
+            catch (Exception e)
+            {
+                DevLog.LogError($"init load failed : {e.Message}");
+            }
+        }
+
+#endregion
+
+#region Exist
+
+        public bool SlotDataExist(int slot)
+        {
+            if (slot < 0 || slot >= MaxSaveSlot)
+            {
+                DevLog.LogError("invalid save slot");
+                return false;
+            }
+            return _slotDataExist[slot];
+        }
+        
+        public bool AutoDataExist()
+        {
+            return autoDataExist;
+        }
+
+#endregion
+
+#region Save
+
+        public void SaveData(string key, int value)
+        {
+            _currentData[key] = value;
+        }
+                
+        public async Task SlotSave(int slot)
+        {
+            if (slot < 0 || slot >= MaxSaveSlot)
+            {
+                return;
+            }
+            
+            try
+            {
+                string jsonData = DictionaryJsonConvert.ToJson(_currentData, true);
+                await File.WriteAllTextAsync($"{_path}/save_{slot}.json", jsonData);
+                allData[slot] = new Dictionary<string, int>(_currentData);
+                _slotDataExist[slot] = true;
+            }
+            catch (Exception e)
+            {
+                DevLog.LogError($"save failed: {e.Message}");
+            }
+        }
+
+        public async Task AutoSave()
+        {
+            try
+            {
+                _autoSaved = new Dictionary<string, int>(_currentData);
+                string jsonData = DictionaryJsonConvert.ToJson(_autoSaved, true);
+                await File.WriteAllTextAsync($"{_path}/save_auto.json", jsonData);
+                autoDataExist = true;
+            }
+            catch (Exception e)
+            {
+                DevLog.LogError($"save failed: {e.Message}");
+            }
+        }
+
+#endregion
+
+#region Load
+
+        public bool TryGetValue(string key, out int value)
+        {
+            return _currentData.TryGetValue(key, out value);
+        }
+
+        public Dictionary<string, int> LoadSlot(int slot)
+        {
+            if(slot < 0 || slot >= MaxSaveSlot)
+            {
+                DevLog.LogError("invalid save slot");
+                return null;
+            }
+            _currentData = new Dictionary<string, int>(allData[slot]);
+            return new Dictionary<string, int>(allData[slot]);
+        }
+        
+        public Dictionary<string, int> LoadAutoSave()
+        {
+            _currentData = new Dictionary<string, int>(_autoSaved);
+            return new Dictionary<string, int>(_autoSaved);
+        }
+        
+        public List<Dictionary<string, int>> GetAllData()
+        {
+            var list = new List<Dictionary<string, int>>();
+            foreach (var data in allData)
+            {
+                list.Add(new Dictionary<string, int>(data));
+            }
+            return new (list);
+        }
+
+#endregion
+    }
+
+    public class DictionaryJsonConvert
+    {
+        public static string ToJson<TKey, TValue>(Dictionary<TKey, TValue> jsonDicData, bool pretty = false)
+        {
+            List<DataDictionary<TKey, TValue>> dataList = new List<DataDictionary<TKey, TValue>>();
+            DataDictionary<TKey, TValue> dictionaryData;
+            foreach (TKey key in jsonDicData.Keys)
+            {
+                dictionaryData = new DataDictionary<TKey, TValue>();
+                dictionaryData.Key = key;
+                dictionaryData.Value = jsonDicData[key];
+                dataList.Add(dictionaryData);
+            }
+            JsonDataArray<TKey, TValue> arrayJson = new JsonDataArray<TKey, TValue>();
+            arrayJson.data = dataList;
+
+            return JsonUtility.ToJson(arrayJson, pretty);
+        }
+        
+        public static Dictionary<TKey, TValue> FromJson<TKey, TValue>(string jsonData)
+        {
+            JsonDataArray<TKey, TValue> dataList = JsonUtility.FromJson<JsonDataArray<TKey, TValue>>(jsonData);
+            
+            Dictionary<TKey, TValue> returnDictionary = new Dictionary<TKey, TValue>();
+            
+            foreach (var dictionaryData in dataList.data)
+            {
+                returnDictionary[dictionaryData.Key] = dictionaryData.Value;
+            }
+            
+            return returnDictionary;
+        }
+    }
+    
+    [Serializable]
+    public class DataDictionary<TKey,TValue>
+    {
+        public TKey Key;
+        public TValue Value;
+    }
+
+    [Serializable]
+    public class JsonDataArray<TKey, TValue>
+    {
+        public List<DataDictionary<TKey, TValue>> data;
+    }
+}
