9e5d0a2e9d1ed89c7dcb9513352c4b93449773ca
djEjsdlfma <asdakpol.asd@gmail.com>
2026-04-30 00:46:53 +0900

    feat : Save Link
    세이브하고 대충 연결짓기

--- Assets/RSJ/Script/Title/LogContainer.cs
+++ Assets/RSJ/Script/Title/LogContainer.cs
@@ -1,4 +1,5 @@
 using Moon._01.Script.Datas;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,11 +11,14 @@ public class LogContainer : MonoBehaviour
     [SerializeField] private Button Btn1to10;
     [SerializeField] private Button Btn11to20;
 
+    [SerializeField] private Button BtnSave;
+    [SerializeField] private Button BtnRoad;
+
     private DataManager _data;
 
     public void Awake()
     {
-        _data = GetComponent<DataManager>();
+        _data = DataManager.Instance;
         Btn1to10.interactable = false;
 
         for (int i = 0; i < List11to20.Length; i++)
@@ -25,16 +29,12 @@ public class LogContainer : MonoBehaviour
 
     public void Start()
     {
-        for (int i = 0; i < List11to20.Length; i++)
-        {
-            int index = i; // 클로저 문제 방지
-            List11to20[i].gameObject.GetComponent<Button>().onClick
-                .AddListener(() => _data.SlotSave(index));
-        }
-        for (int i = 0; i < List1to10.Length; i++)
-        {
-            List1to10[i].gameObject.SetActive(false);
-        }
+        SetSaveState();
+    }
+
+    private void OnDestroy()
+    {
+        RemoveAll();
     }
 
     public void ChangeList()
@@ -50,8 +50,10 @@ public class LogContainer : MonoBehaviour
                 List1to10[i].gameObject.SetActive(false);
             }
 
+
             Btn1to10.interactable = true;
-            Btn11to20. interactable = false;
+            Btn11to20.interactable = false;
+
         }
         else
         {
@@ -66,9 +68,99 @@ public class LogContainer : MonoBehaviour
 
             Btn1to10.interactable = false;
             Btn11to20.interactable = true;
-        }
+        } 
+    }
 
-        
+    public void ChangeState()
+    {
+        RemoveAll();
+        if (BtnRoad.interactable == true)
+        {
+            SetSaveState();
+
+
+            BtnSave.interactable = true;
+            BtnRoad.interactable = false;
+        }
+        else
+        {
+            SetRoadState();
+
+            BtnSave.interactable = false;
+            BtnRoad.interactable = true;
+        }
+    }
+
+    private void SetRoadState()
+    {
+        for (int i = 0; i < List11to20.Length; i++)
+        {
+            int index = i; // 클로저 문제 방지
+            List11to20[i].gameObject.GetComponent<Button>().onClick
+                .AddListener(() =>
+                {
+                if (_data.SlotDataExist(index))
+                    _data.LoadSlot(index);
+                });
+        }
+        for (int i = 0; i < List1to10.Length; i++)
+        {
+            int index = i; // 클로저 문제 방지
+            List1to10[i].gameObject.GetComponent<Button>().onClick
+                .AddListener(() =>
+                {
+                    if (_data.SlotDataExist(index))
+                        _data.LoadSlot(index);
+                });
+        }
+    }
+
+    private void SetSaveState()
+    {
+        for (int i = 0; i < List11to20.Length; i++)
+        {
+            int index = i; // 클로저 문제 방지
+            List11to20[i].gameObject.GetComponent<Button>().onClick
+                .AddListener(() =>
+                {
+                    _data.SlotSave(index);
+
+                    List11to20[index].gameObject.GetComponent<LogUI>()
+                        .CheckData(_data.SlotDataExist(index));
+                });
+
+            List11to20[index].gameObject.GetComponent<LogUI>()
+                .CheckData(_data.SlotDataExist(index));
+        }
+        for (int i = 0; i < List1to10.Length; i++)
+        {
+            int index = i; // 클로저 문제 방지
+            List1to10[i].gameObject.GetComponent<Button>().onClick
+                .AddListener(() =>
+                {
+                    _data.SlotSave(index);
+
+                    List1to10[index].gameObject.GetComponent<LogUI>()
+                        .CheckData(_data.SlotDataExist(index));
+                });
+
+            List1to10[index].gameObject.GetComponent<LogUI>()
+                .CheckData(_data.SlotDataExist(index));
+        }
+    }
+
+    public void RemoveAll()
+    {
+        for (int i = 0; i < List11to20.Length; i++)
+        {
+            List11to20[i].gameObject.GetComponent<Button>().onClick
+                .RemoveAllListeners();
+        }
+        for (int i = 0; i < List1to10.Length; i++)
+        {
+            List1to10[i].gameObject.GetComponent<Button>().onClick
+                .RemoveAllListeners();
+        }
     }
 }
 
