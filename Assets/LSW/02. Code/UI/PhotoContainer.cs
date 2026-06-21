b30a2abab85dc86966af3391b5c3f7ca9e9a1e96
LSW <leemsh1236@gmail.com>
2026-06-21 23:47:37 +0900

    Fix : Photo Container

--- Assets/LSW/02. Code/UI/PhotoContainer.cs
+++ Assets/LSW/02. Code/UI/PhotoContainer.cs
@@ -27,18 +27,42 @@ namespace LSW._02._Code.UI
 
         public void SetPhoto(Guest guest)
         {
-            string guestKey = CurrentGuestManager.C[(int)guest];
+            ClearGeneratedSprites();
+
+            string guestKey = CurrentGuestManager.C[(int)guest - 1];
+            
             if (DataManager.Instance.TryGetValue(guestKey, out List<Texture2D> photoList) && photoList.Count > 0)
             {
+                noPhotoText.gameObject.SetActive(false);
+                PhotoCount = Mathf.Min(photoList.Count, photoImages.Count);
+
                 for (int i = 0; i < photoImages.Count; i++)
                 {
                     if (i < photoList.Count)
                     {
                         photoImages[i].gameObject.SetActive(true);
-                        photoImages[i].sprite = Sprite.Create(photoList[i], new Rect(0, 0, photoList[i].width, photoList[i].height),
-                            new Vector2(0.5f, 0.5f));
+                        
+                        photoImages[i].sprite = Sprite.Create(
+                            photoList[i], 
+                            new Rect(0, 0, photoList[i].width, photoList[i].height),
+                            new Vector2(0.5f, 0.5f), 
+                            100.0f
+                        );
                     }
                     else
+                    {
+                        photoImages[i].gameObject.SetActive(false);
+                    }
+                }
+            }
+            else
+            {
+                noPhotoText.gameObject.SetActive(true);
+                PhotoCount = 0;
+
+                for (int i = 0; i < photoImages.Count; i++)
+                {
+                    if (photoImages[i] != null)
                     {
                         photoImages[i].gameObject.SetActive(false);
                     }
